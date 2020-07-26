64f11aa049a764519e2a353df89ebdc712e3d541
Ross MacDonald <rmac2289@gmail.com>
2020-07-26 02:55:15 -0700

    added map marker callout

---
+++ Comments.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+
+
+export default function Comments(){
+    return (
+    <View style={styles.comments}>
+        <View style={styles.textBox}>
+            <Text style={styles.text}></Text>
+        </View>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
--- Map.js
+++ Map.js
@@ -1,4 +1,4 @@
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import React, { useContext } from 'react';
 import tree from './images/tree.png';
 import { LoginContext } from './Contexts/LoginContext'
@@ -7,17 +7,33 @@ import {
     Dimensions,
     View,
     TouchableOpacity,
-    Text
+    Text,
+    Image
   } from 'react-native';
 import parks from './data';
 import Footer from './Footer';
 import { useNavigation } from '@react-navigation/native';
+import { FullParkNameContext } from './Contexts/ParkNameContext';
 
 export default function MapUI() {
+  const [fullParkName, setFullParkName] = useContext(FullParkNameContext)
     const navigation = useNavigation();
   const [loggedIn, setLoggedIn] = useContext(LoginContext)
     const markers = parks.data.map((v,i) => {
-        return <Marker title={v.fullName} description={v.hours} image={tree} key={i} coordinate={{ latitude: parseFloat(v.latLng[0]), longitude: parseFloat(v.latLng[1]) }}/>
+        return <Marker calloutOffset={-5,-5} image={tree} key={i} coordinate={{ latitude: parseFloat(v.latLng[0]), longitude: parseFloat(v.latLng[1]) }}>
+                <Callout onPress={() => {
+                  setFullParkName(v.fullName);
+                  navigation.navigate('Park');
+                }}style={styles.callout} tooltip={false}>
+                  <View style={styles.calloutBox}>
+                      <Text style={styles.calloutHeader}>{v.fullName}</Text>
+                      <Text style={styles.calloutText}>Hours: <Text style={styles.lightText}>{v.hours}</Text></Text>
+                      <View style={styles.imageBox}>
+                      <Image style={{height: 100, width: 150}} source={{uri: v.images[0].url}}/>
+                      </View>
+                  </View>
+                </Callout>
+          </Marker>
     })
     return (
       <>
@@ -71,6 +87,33 @@ export default function MapUI() {
 }
 
 const styles = StyleSheet.create({
+  imageBox: {
+    alignItems: "center"
+  },
+  callout: {
+    backgroundColor: "rgba(255,255,255,0.8)",
+    position: "absolute",
+    maxHeight: 600,
+    borderWidth: 3,
+    borderColor: "#414f47",
+    padding: 10,
+    borderRadius: 5
+  },
+  calloutHeader: {
+    color: "#414f47",
+    fontWeight: "800",
+    fontFamily: "Avenir",
+    maxWidth: 200
+  },
+  calloutText: {
+    color: "#414f47",
+    fontWeight: "900",
+    maxWidth: 200,
+    fontFamily: "Avenir"
+  },
+  lightText: {
+    fontWeight: "normal"
+  },
   nav: {
     height: 80,
     backgroundColor: '#414f47',
